f0a836f84cba229805a6d390520bbb53fc3170e3
Arin Jeong <arinj9508@gmail.com>
2020-02-28 14:09:41 +0900

    (수정) 2.28 알고리즘 문제풀이

--- 2.28/장훈이의 높은 선반.py
+++ 2.28/장훈이의 높은 선반.py
@@ -1,5 +1,6 @@
-def f(n,k):
-    if n == k:
+# 부분집합을 만들어서 사용한 풀이
+def f(n, k):
+    if n == k:  # 부분집합 중 1개 완성
         for i in range(k):
             if L[i] == 1:
                 s.append(N_H[i])
@@ -7,19 +8,22 @@ def f(n,k):
         del s[:]
     else:
         L[n] = 0
-        f(n+1, k)
+        f(n + 1, k)
         L[n] = 1
-        f(n+1, k)
+        f(n + 1, k)
+
 
 T = int(input())
-for tc in range(1, T+1):
-    N, B = map(int, input().split())
-    N_H = list(map(int, input().split()))
+for tc in range(1, T + 1):
+    N, B = map(int, input().split())  # 1 ≤ N(점원수) ≤ 20, 1 ≤ B(선반높이) ≤ S(점원키)
+    N_H = list(map(int, input().split()))  # 1 ≤ N_H ≤ 10,000
 
-    L = [0]*N
+    L = [0] * N
     s = []
     lst = []
-    f(0,N)
+    f(0, N)
+
+    # print(lst)
 
     minn = []
     for i in lst:
